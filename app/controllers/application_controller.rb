013e65ef835bc105a3f80b382bcefbf59ee1d005
alextaylor <alextaylor515@gmail.com>
2022-08-11 14:16:28 -0400

    commit tree 885fa182cc193073be11a195e05b606521b789c0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid_error
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+    private 
+    def handle_invalid_error
+        render json: {error: "You fucked up!"},status: :unprocessable_entity
+    end
+    def handle_not_found
+        render json: {error: "Not found"}, status: 404
+    end
+    
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id] 
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
 
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+  
+  def create
+    if params[:user_id]
+      item = Item.create!(paramsallowed)
+    else
+      item = Item.create!(paramsallowed)
+    end
+    render json: item, status: 201
+  end
+
+  private
+
+  def paramsallowed
+    params.permit(:price,:name,:description,:user_id)
+  end
 end
